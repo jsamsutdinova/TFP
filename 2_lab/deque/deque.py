f0e86ae3516fa2154bfaf1b12794d35a5a3b1da1
Julia Shamsutdinova <shjn0297@gmail.com>
2020-09-27 16:26:44 +0800

    Created Class Deque

---
+++ 2_lab/deque/deque.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/env python3
+""" The class for laboratory work 2 """
+
+class Deque:
+    """ This class simulates work of deque """
+
+    def __init__(self, deque):
+        self.deque = deque
+        
