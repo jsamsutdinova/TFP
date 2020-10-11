066a352ed9bf80ed6e7ac4647233519ca11172ef
Julia Shamsutdinova <shjn0297@gmail.com>
2020-10-11 15:19:07 +0800

    Fixed name of objects

--- 3_lab/task_3.py
+++ 3_lab/task_3.py
@@ -2,7 +2,7 @@
 """ Laboratory work 3. Task 3 """
 
 class HashTable:
-    """ 
+    """
         This class implements operations of a hash table.
         Use linear probling to avoid collision
     """
@@ -12,17 +12,22 @@ class HashTable:
         self.slots = [None] * self.size
         self.data = [None] * self.size
 
-    def countHash (self, key, size):
+    @classmethod
+    def count_hash (cls, key, size):
+        """ Get hash key using remainder of a division """
         return key%size
-    
-    def linearProbling (self, old_hash, size):
+
+    @classmethod
+    def linear_probling (cls, old_hash, size):
+        """ Solve collision problem """
         return (old_hash+1)%size
-    
-    def addItem (self, key, data):
-        hash_key = self.countHash(key, len(self.slots))
+
+    def add_item (self, key, data):
+        """ Find empty slot and add new element to this slot"""
+        hash_key = self.count_hash(key, len(self.slots))
 
         # Условие для случая, когда место в слоте пустое
-        if self.slots[hash_key] == None:
+        if self.slots[hash_key] is None:
             self.slots[hash_key] = key
             self.data[hash_key] = data
         else:
@@ -31,12 +36,12 @@ class HashTable:
                 self.data[hash_key] = data
             else:
                 # Иначе применяем линейное пробирование для избежания коллизии
-                new_hash_key = self.linearProbling(hash_key, len(self.slots))
-            
-                while self.slots[new_hash_key] != None and self.slots[new_hash_key] != key:
-                    new_hash_key = self.linearProbling(new_hash_key, len(self.slots))
+                new_hash_key = self.linear_probling(hash_key, len(self.slots))
 
-                if self.slots[new_hash_key] == None:
+                while self.slots[new_hash_key] is not None and self.slots[new_hash_key] != key:
+                    new_hash_key = self.linear_probling(new_hash_key, len(self.slots))
+
+                if self.slots[new_hash_key] is None:
                     # Добавляем в пустой слот значение ключа и данные
                     self.slots[new_hash_key] = key
                     self.data[new_hash_key] = data
@@ -44,9 +49,10 @@ class HashTable:
                     # Если такой ключ уже есть, то обновляем данные
                     self.data[new_hash_key] = data
 
-    def __additem__ (self, key, data):
-        self.addItem(key, data)
+
+    def __setitem__(self, key, data):
+        """ Special function for [] constraction """
+        self.add_item(key, data)
 
 if __name__ == '__main__':
     hash_table = HashTable()
-    
