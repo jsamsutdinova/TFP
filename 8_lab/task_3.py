61f6ded65db3436d93a014fa31b7aa3e8c7e8cc3
Julia Shamsutdinova <shjn0297@gmail.com>
2020-12-25 23:01:25 +0800

    Renamed parameters

--- 8_lab/task_3.py
+++ 8_lab/task_3.py
@@ -2,11 +2,11 @@ import json
 
 
 class Bouquet():
-    def __init__(self, shared_state):
-        self._shared_state = shared_state
+    def __init__(self, flowers):
+        self._flowers = flowers
 
-    def operation(self, unique_state):
-        u = json.dumps(unique_state)
+    def operation(self, attribute):
+        u = json.dumps(attribute)
         print(f"Добавим атрибут ({u}).", end="\n")
 
 
@@ -14,25 +14,25 @@ class BouquetFactory():
 
     def __init__(self, init_bouquet):
         self._bouquets = {}
-        for state in init_bouquet:
-            self._bouquets[self.get_key(state)] = Bouquet(state)
+        for flower in init_bouquet:
+            self._bouquets[self.get_key(flower)] = Bouquet(flower)
 
-    def get_key(self, state):
-        return "".join((state))
+    def get_key(self, flower):
+        return "".join((flower))
 
-    def get_flyweight(self, shared_state):
-        key = self.get_key(shared_state)
+    def get_attribute(self, attribute):
+        key = self.get_key(attribute)
 
         if not self._bouquets.get(key):
-            self._bouquets[key] = Bouquet(shared_state)
+            self._bouquets[key] = Bouquet(attribute)
 
         return self._bouquets[key]
 
     def add_attribute(self, attribute_name):
-        flowers = self.get_flyweight([attribute_name])
+        flowers = self.get_attribute([attribute_name])
         flowers.operation([attribute_name])
 
-    def list_flyweights(self):
+    def list_flowers(self):
         count = len(self._bouquets)
         print(f"Букет состоит из {count} элементов:")
         print("\n".join(map(str, self._bouquets.keys())), end="\n")
@@ -41,9 +41,9 @@ class BouquetFactory():
 if __name__ == "__main__":
     flowers1 = BouquetFactory(["Розы", "Герберы"])
     flowers1.add_attribute("Personal Lenta")
-    flowers1.list_flyweights()
+    flowers1.list_flowers()
 
     flowers2 = BouquetFactory(["Гартензии", "Тюльпаны", "Ирисы"])
     flowers2.add_attribute("Wrapping Paper")
-    flowers2.list_flyweights()
+    flowers2.list_flowers()
 
